ac99f21771d2abe284847ace19e8a4bb878111b9
kimdo2da <107172886+kimdo2da@users.noreply.github.com>
2026-03-10 22:05:39 +0900

    Fix API URL construction in aladin.js
    api 삭제

--- api/aladin.js
+++ api/aladin.js
@@ -1,7 +1,7 @@
 // 서버리스 함수: api/aladin.js (루트 경로에 api 폴더 만들고 안에 넣어)
 export default async function handler(req, res) {
   const query = req.url.split('?')[1];
-  const apiUrl = `https://www.aladin.co.kr/ttb/api/ItemSearch.aspx?${query}`;
+  
 
   try {
     const response = await fetch(apiUrl);
